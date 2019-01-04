de935394e9e7c0de7ff62fcb611a47f5d06034ec
Mandy <mandy.lan@gmail.com>
2019-01-05 00:09:00 +1300

    first

---
+++ defining_methods.rb
@@ -0,0 +1,23 @@
+#1. write method greet that takes a name, prepends "Hello ", and appends an exclamation mark "!"
+def greet(name)
+  greetings = ["Hello", "Hi", "Ohai", "ZOMG"].shuffle
+  "#{greetings.first} #{name}!"
+  end
+
+puts greet("Ada")
+
+#2.write a method that converts miles to km
+def miles_to_kilometers(miles)
+  miles * 1.6
+end
+
+puts miles_to_kilometers(25)
+
+#3.write a method that takes a year  and calculates if it's a leap year
+def leap_year?(year)
+  year % 4 == 0
+end
+
+p leap_year?(2012)
+p leap_year?(2015)
+
