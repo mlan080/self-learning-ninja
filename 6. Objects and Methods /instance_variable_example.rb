bc881cc5318fca54f7da034b789582309fab120e
Mandy <mandy.lan@gmail.com>
2019-01-08 20:28:51 +1300

    additional example

--- 6. Objects and Methods /instance_variable_example.rb
+++ 6. Objects and Methods /instance_variable_example.rb
@@ -6,17 +6,16 @@ class Customer
       @cust_name = name
       @cust_addr = addr
    end
-   def display_details()
-      puts "Customer id #@cust_id"
-      puts "Customer name #@cust_name"
-      puts "Customer address #@cust_addr"
-   end
-end
+
+   def to_s
+    "(#{@cust_id}, #{@cust_id}, #{@cust_id})"
+    end
+  end
 
 # Create Objects
 cust1 = Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
 cust2 = Customer.new("2", "Poul", "New Empire road, Khandala")
 
 # Call Methods
-cust1.display_details()
-cust2.display_details()
+puts cust1
+puts cust2
