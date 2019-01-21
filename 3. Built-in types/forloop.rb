135673651ca50c1272e1c85ad66cd899584875f0
Mandy <mandy.lan@gmail.com>
2019-01-21 11:09:23 +0100

    test forloop

--- 3. Built-in types/forloop.rb
+++ 3. Built-in types/forloop.rb
@@ -8,3 +8,22 @@ while x <= 10 do
   x = 0
   x += 1 while x < 10
   p x
+
+#Using the loop construct, add the current value
+#of number to the numbers array. Inside of the loop
+# add 1 to the number variable. If the numbers array
+#has more than 3 items, use the break keyword to exit the loop."
+
+numbers = []
+
+number = 0
+
+loop do
+  number += 1
+  numbers.push(number)
+  if numbers.length >=3
+  break
+  end
+end
+
+puts numbers
